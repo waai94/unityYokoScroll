1e4a7fd8d5f5c4ec5a418079a0ce768f1e3137b9
LAPTOP-92372506\admin <waai21899@gmail.com>
2025-11-20 09:37:56 +0900

    UIの仮実装

---
+++ yokobattle/Assets/Script/Widgets/NameUIManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+/// <summary>
+/// 対象のキャラクターの名前をUIに表示するスクリプト
+/// 
+/// </summary>
+public class NameUIManager : MonoBehaviour
+{
+    enum whichName
+    {
+        Player,
+        Enemy
+    }
+
+    [SerializeField] private whichName nameType = whichName.Player;//名前の種類を選択する列挙型
+
+    TextMeshPro TextMeshPro;//TextMeshProコンポーネントへの参照
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TextMeshPro = GetComponent<TextMeshPro>();
+        if(!TextMeshPro)
+        {
+            Debug.LogError("TextMeshPro component not found on " + gameObject.name);
+            return;
+        }
+
+        string characterName = GetName();
+        TextMeshPro.text = characterName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    string GetName()
+    {
+        string name = "";
+        string findTag = "";
+        switch(nameType)
+        {
+            case whichName.Player:
+                findTag = "Player";
+                break;
+            case whichName.Enemy:
+                findTag = "Enemy";
+                break;
+        }
+
+        GameObject character = GameObject.FindGameObjectWithTag(findTag);
+       if(!character)return $"{findTag} not found";
+        CharacterInfoScript info = character.GetComponent<CharacterInfoScript>();
+        if(!info)
+        {
+            info = character.GetComponentInChildren<CharacterInfoScript>(); //子オブジェクトにある場合
+        }
+        if(!info)return $"{findTag} CharacterInfoScript not found";
+        name = info.CharacterName;
+        return name;
+    }
+}
